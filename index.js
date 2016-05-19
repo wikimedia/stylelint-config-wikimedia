ebdfd46a25c8048aba3e38408d41c044bd2f239e
Volker E <Volker-E@users.noreply.github.com>
2016-05-19 00:45:42 -0700

    Fix #4, #5 & #6
    Bringing logical order by ordering alphabetically and structure into either a new rule group or when subgroups are including more than 3 items.
    Removing `block-closing-brace-space-after` and `string-quotes` rule for now, as described in the corresponding issues. Also fixing small typo in `at-rule-empty-line-before` rule.

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 module.exports = {
 	"rules": {
 
-		"indentation": [ "tab" ],
-		"max-empty-lines": 1,
-		"no-eol-whitespace": true,
-		"no-missing-eof-newline": true,
+		"at-rule-empty-line-before": [ "always", {
+			except: [ "blockless-group", "first-nested" ],
+			ignore: [ "after-comment" ],
+		} ],
+		"at-rule-name-case": "lower",
+		"at-rule-name-space-after": "always-single-line",
 
-		"no-duplicate-selectors": true,
-		"no-descending-specificity": true,
-		"declaration-no-important": true,
-		"selector-no-id": true,
+		"block-closing-brace-newline-after": [ "always" ],
+		"block-closing-brace-newline-before": [ "always-multi-line" ],
+		"block-closing-brace-space-before": [ "always-single-line" ],
 
-		"no-unknown-animations": true,
+		"block-no-empty": true,
+		"block-no-single-line": true,
+
+		"block-opening-brace-newline-after": [ "always-multi-line" ],
+		"block-opening-brace-space-after": [ "always-single-line" ],
+		"block-opening-brace-space-before": [ "always" ],
 
 		"color-hex-case": [ "lower" ],
 		"color-hex-length": [ "short" ],
 		"color-named": [ "never" ],
 		"color-no-invalid-hex": true,
+
+		"declaration-bang-space-after": [ "never" ],
+		"declaration-bang-space-before": [ "always" ],
+
+		"declaration-block-no-duplicate-properties": [ true, { ignore: [ "consecutive-duplicates" ] } ],
+		"declaration-block-no-shorthand-property-overrides": true,
+		"declaration-block-semicolon-newline-after": [ "always" ],
+		"declaration-block-semicolon-newline-before": [ "never-multi-line" ],
+		"declaration-block-semicolon-space-after": [ "always-single-line" ],
+		"declaration-block-semicolon-space-before": [ "never" ],
+		"declaration-block-single-line-max-declarations": 1,
+		"declaration-block-trailing-semicolon": [ "always" ],
+
+		"declaration-colon-space-after": [ "always" ],
+		"declaration-colon-space-before": [ "never" ],
+		"declaration-no-important": true,
 
 		"font-family-name-quotes": [ "single-unless-keyword" ],
 		"font-weight-notation": [ "named-where-possible" ],
@@ -31,49 +53,8 @@ module.exports = {
 		"function-url-quotes": [ "none" ],
 		"function-whitespace-after": [ "always" ],
 
-		"value-list-comma-newline-after": [ "never-multi-line" ],
-		"value-list-comma-newline-before": [ "always-multi-line" ],
-		"value-list-comma-space-after": [ "always-single-line" ],
-		"value-list-comma-space-before": [ "never" ],
-
-		"declaration-bang-space-after": [ "never" ],
-		"declaration-bang-space-before": [ "always" ],
-		"declaration-colon-space-after": [ "always" ],
-		"declaration-colon-space-before": [ "never" ],
-
-		"string-no-newline": true,
-		"string-quotes": [ "single" ],
-
-		"declaration-block-no-duplicate-properties": [ true, { ignore: [ "consecutive-duplicates" ] } ],
-		"declaration-block-no-shorthand-property-overrides": true,
-
-		"declaration-block-semicolon-newline-after": [ "always" ],
-		"declaration-block-semicolon-newline-before": [ "never-multi-line" ],
-		"declaration-block-semicolon-space-after": [ "always-single-line" ],
-		"declaration-block-semicolon-space-before": [ "never" ],
-		"declaration-block-single-line-max-declarations": 1,
-		"declaration-block-trailing-semicolon": [ "always" ],
-
-		"block-closing-brace-newline-after": [ "always" ],
-		"block-closing-brace-newline-before": [ "always-multi-line" ],
-		"block-closing-brace-space-after": [ "always-single-line" ],
-		"block-closing-brace-space-before": [ "always-single-line" ],
-		"block-no-empty": true,
-		"block-no-single-line": true,
-		"block-opening-brace-newline-after": [ "always-multi-line" ],
-		"block-opening-brace-space-after": [ "always-single-line" ],
-		"block-opening-brace-space-before": [ "always" ],
-
-		"selector-combinator-space-after": [ "always" ],
-		"selector-combinator-space-before": [ "always" ],
-		"selector-no-vendor-prefix": true,
-		"selector-root-no-composition": true,
-		"selector-pseudo-element-colon-notation": [ "double" ],
-
-		"selector-list-comma-newline-after": [ "always" ],
-		"selector-list-comma-newline-before": [ "never-multi-line" ],
-		"selector-list-comma-space-after": [ "always-single-line" ],
-		"selector-list-comma-space-before": [ "never" ],
+		"indentation": [ "tab" ],
+		"max-empty-lines": 1,
 
 		"media-feature-colon-space-after": [ "always" ],
 		"media-feature-colon-space-before": [ "never" ],
@@ -89,12 +70,30 @@ module.exports = {
 		"media-query-list-comma-space-after": [ "always-single-line" ],
 		"media-query-list-comma-space-before": [ "never" ],
 
-		"at-rule-empty-line-before": [ "always", {
-			except: [ "blockless-group", "first-nested" ],
-			ignore: ["after-comment"],
-		} ],
-		"at-rule-name-case": "lower",
-		"at-rule-name-space-after": "always-single-line",
+		"no-eol-whitespace": true,
+		"no-missing-eof-newline": true,
+		"no-duplicate-selectors": true,
+		"no-descending-specificity": true,
+		"no-unknown-animations": true,
+
+		"value-list-comma-newline-after": [ "never-multi-line" ],
+		"value-list-comma-newline-before": [ "always-multi-line" ],
+		"value-list-comma-space-after": [ "always-single-line" ],
+		"value-list-comma-space-before": [ "never" ],
+
+		"string-no-newline": true,
+
+		"selector-combinator-space-after": [ "always" ],
+		"selector-combinator-space-before": [ "always" ],
+		"selector-no-id": true,
+		"selector-no-vendor-prefix": true,
+		"selector-root-no-composition": true,
+		"selector-pseudo-element-colon-notation": [ "double" ],
+
+		"selector-list-comma-newline-after": [ "always" ],
+		"selector-list-comma-newline-before": [ "never-multi-line" ],
+		"selector-list-comma-space-after": [ "always-single-line" ],
+		"selector-list-comma-space-before": [ "never" ]
 
 	}
 }
