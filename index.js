3a042db74ace36e5000c8bca6fa9a0409a6870c6
Volker E <Volker-E@users.noreply.github.com>
2016-10-05 14:11:06 -0700

    Add outline to property value blacklist
    Adding `outline: none` to declaration-property-value-blacklist.
    
    Fixing #21

--- index.js
+++ index.js
@@ -104,7 +104,8 @@ module.exports = {
 		"at-rule-no-unknown": true,
 
 		"declaration-property-value-blacklist": {
-			"/^border/": [ "none" ]
+			"/^border/": [ "none" ],
+			"/^outline/": [ "none" ]
 		}
 	}
 };
