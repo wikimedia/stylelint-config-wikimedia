932ecfe6030c4cf578923cdb53e2068e2c91f3c2
Volker E <Volker-E@users.noreply.github.com>
2016-10-07 15:18:26 -0700

    Fix rule value
    Fixing `at-rule-semicolon-newline-after` to be a string `[ "always" ]` instead of a binary value.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ module.exports = {
 		"at-rule-name-case": "lower",
 		"at-rule-name-space-after": "always-single-line",
 		"at-rule-no-unknown": true,
-		"at-rule-semicolon-newline-after": true,
+		"at-rule-semicolon-newline-after": [ "always" ],
 
 		"block-closing-brace-newline-after": [ "always" ],
 		"block-closing-brace-newline-before": [ "always-multi-line" ],
