849e0e9939a9b900bff267172157c04e4b133967
Ed S <ejsanders@gmail.com>
2017-08-03 18:26:14 -0700

    Upgrade to stylelint 7.12 (#60)
    Fixes #59

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ module.exports = {
 		"selector-list-comma-space-after": [ "always-single-line" ],
 		"selector-list-comma-space-before": [ "never" ],
 
-		"selector-no-id": true,
+		"selector-max-id": 0,
 		"selector-no-vendor-prefix": true,
 		"selector-pseudo-class-case": [ "lower" ],
 		"selector-pseudo-class-no-unknown": true,
