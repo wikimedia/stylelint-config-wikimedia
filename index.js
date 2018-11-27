8352e91af28ad9161de0f0d18af7a599e022f9d7
Volker E <Volker-E@users.noreply.github.com>
2018-11-27 13:47:48 -0800

    New rule: `comment-whitespace-inside`, set to `always`
    Part of #3

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ module.exports = {
 		"color-no-invalid-hex": true,
 
 		"comment-no-empty": true,
+		"comment-whitespace-inside": "always",
 
 		"declaration-bang-space-after": [ "never" ],
 		"declaration-bang-space-before": [ "always" ],
