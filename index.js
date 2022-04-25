0a87822cdbc4d4018928ff5ccbe25e5f1f34e03b
Ed S <ejsanders@gmail.com>
2022-04-25 13:18:17 +0100

    Extend stylelint-config-recommended (#174)
    Introduces the following rules:
    * custom-property-no-missing-var-function
    * declaration-block-no-duplicate-custom-properties
    * font-family-no-duplicate-names
    * keyframe-declaration-no-important
    * named-grid-areas-no-invalid
    * no-duplicate-at-import-rules
    * no-empty-source
    * no-invalid-position-at-import-rule
    * no-irregular-whitespace
    * selector-pseudo-element-no-unknown
    
    Remove rules which are duplicated upstream
    
    declaration-block-no-duplicate-properties is changed slightly
    from ignore:consecutive-duplicates to
    ignore:consecutive-duplicates-with-different-values.
    
    Fixes #173

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 /* eslint quotes: ["error", "double"] */
 /* eslint quote-props: ["error", "always"] */
 module.exports = {
+	"extends": "stylelint-config-recommended",
 	"rules": {
 		// Wikimedia Foundation ♡ whitespace in its own special way
 		// See also https://www.mediawiki.org/wiki/Manual:Coding_conventions/CSS#Whitespace
@@ -22,7 +23,6 @@ module.exports = {
 		} ],
 		"at-rule-name-case": "lower",
 		"at-rule-name-space-after": "always-single-line",
-		"at-rule-no-unknown": true,
 		"at-rule-semicolon-newline-after": "always",
 
 		"block-closing-brace-empty-line-before": "never",
@@ -30,8 +30,6 @@ module.exports = {
 		"block-closing-brace-newline-before": "always-multi-line",
 		"block-closing-brace-space-after": "always-single-line",
 		"block-closing-brace-space-before": "always-single-line",
-
-		"block-no-empty": true,
 
 		"block-opening-brace-newline-after": "always",
 		"block-opening-brace-newline-before": "always-single-line",
@@ -41,20 +39,13 @@ module.exports = {
 		"color-hex-case": "lower",
 		"color-hex-length": "short",
 		"color-named": "never",
-		"color-no-invalid-hex": true,
 
-		"comment-no-empty": true,
 		"comment-whitespace-inside": "always",
 
 		"declaration-bang-space-after": "never",
 		"declaration-bang-space-before": "always",
 
-		"declaration-block-no-duplicate-properties": [ true, {
-			"ignore": "consecutive-duplicates"
-		} ],
 		"declaration-block-no-redundant-longhand-properties": true,
-		"declaration-block-no-shorthand-property-overrides": true,
-
 		"declaration-block-semicolon-newline-after": "always",
 		"declaration-block-semicolon-newline-before": "never-multi-line",
 		"declaration-block-semicolon-space-after": "always-single-line",
@@ -84,16 +75,13 @@ module.exports = {
 		},
 
 		"font-family-name-quotes": "always-unless-keyword",
-		"font-family-no-missing-generic-family-keyword": true,
 		"font-weight-notation": "named-where-possible",
 
 		"function-disallowed-list": "rgb",
-		"function-calc-no-unspaced-operator": true,
 		"function-comma-newline-after": "never-multi-line",
 		"function-comma-newline-before": "never-multi-line",
 		"function-comma-space-after": "always",
 		"function-comma-space-before": "never",
-		"function-linear-gradient-no-nonstandard-direction": true,
 		"function-max-empty-lines": 0,
 		"function-name-case": [ "lower", {
 			"ignoreFunctions": "/^DXImageTransform.Microsoft.*$/"
@@ -110,7 +98,6 @@ module.exports = {
 		"media-feature-colon-space-after": "always",
 		"media-feature-colon-space-before": "never",
 		"media-feature-name-case": "lower",
-		"media-feature-name-no-unknown": true,
 		"media-feature-name-no-vendor-prefix": null,
 		"media-feature-parentheses-space-inside": "always",
 		"media-feature-range-operator-space-after": "always",
@@ -121,17 +108,12 @@ module.exports = {
 		"media-query-list-comma-space-after": "always-single-line",
 		"media-query-list-comma-space-before": "never",
 
-		"no-descending-specificity": true,
-		"no-duplicate-selectors": true,
-		"no-invalid-double-slash-comments": true,
-		"no-extra-semicolons": true,
 		"no-unknown-animations": true,
 
 		"number-leading-zero": "always",
 		"number-no-trailing-zeros": true,
 
 		"property-case": "lower",
-		"property-no-unknown": true,
 
 		"rule-empty-line-before": [
 			"always-multi-line", {
@@ -157,21 +139,18 @@ module.exports = {
 		"selector-max-id": 0,
 		"selector-no-vendor-prefix": true,
 		"selector-pseudo-class-case": "lower",
-		"selector-pseudo-class-no-unknown": true,
 		"selector-pseudo-class-parentheses-space-inside": "always",
 		"selector-pseudo-element-case": "lower",
 		"selector-pseudo-element-colon-notation": "single",
 		"selector-type-case": "lower",
 		"selector-type-no-unknown": true,
 
-		"string-no-newline": true,
 		"string-quotes": "single",
 
 		"time-min-milliseconds": 100,
 
 		"unit-disallowed-list": "rem",
 		"unit-case": "lower",
-		"unit-no-unknown": true,
 
 		"value-keyword-case": "lower",
 		"value-list-max-empty-lines": 0,
