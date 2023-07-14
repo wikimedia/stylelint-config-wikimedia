13f54674372e6e922d6091251c28c7d43b074657
James D. Forrester <jforrester@wikimedia.org>
2023-07-14 11:06:08 -0400

    Upgrade stylelint from 14.16.1 to 15.10.1 (#217)
    This involves a significant change due to upstream moving out most
    (but not all) of their formatting-related rules into a plug-in.
    
    Fixes #202

--- index.js
+++ index.js
@@ -4,15 +4,19 @@
 /* eslint quotes: ["error", "double"] */
 /* eslint quote-props: ["error", "always"] */
 module.exports = {
-	"extends": "stylelint-config-recommended",
+	"extends": [
+		"stylelint-config-recommended",
+		"stylelint-stylistic/config"
+	],
 	"reportNeedlessDisables": true,
 	"rules": {
 		// Wikimedia Foundation ♡ whitespace in its own special way
 		// See also https://www.mediawiki.org/wiki/Manual:Coding_conventions/CSS#Whitespace
-		"indentation": "tab",
-		"max-empty-lines": 1,
-		"no-eol-whitespace": true,
-		"no-missing-end-of-source-newline": true,
+		"stylistic/indentation": "tab",
+		"stylistic/max-empty-lines": 1,
+		"stylistic/max-line-length": null,
+		"stylistic/no-eol-whitespace": true,
+		"stylistic/no-missing-end-of-source-newline": true,
 
 		// Other rules alphabetically
 		"at-rule-empty-line-before": [ "always", {
@@ -22,46 +26,47 @@ module.exports = {
 			],
 			"ignore": "after-comment"
 		} ],
-		"at-rule-name-case": "lower",
-		"at-rule-name-space-after": "always-single-line",
-		"at-rule-semicolon-newline-after": "always",
+		"stylistic/at-rule-name-case": "lower",
+		"stylistic/at-rule-name-space-after": "always-single-line",
+		"stylistic/at-rule-semicolon-newline-after": "always",
 
-		"block-closing-brace-empty-line-before": "never",
-		"block-closing-brace-newline-after": "always",
-		"block-closing-brace-newline-before": "always-multi-line",
-		"block-closing-brace-space-after": "always-single-line",
-		"block-closing-brace-space-before": "always-single-line",
+		"stylistic/block-closing-brace-empty-line-before": "never",
+		"stylistic/block-closing-brace-newline-after": "always",
+		"stylistic/block-closing-brace-newline-before": "always-multi-line",
+		"stylistic/block-closing-brace-space-after": "always-single-line",
+		"stylistic/block-closing-brace-space-before": "always-single-line",
 
-		"block-opening-brace-newline-after": "always",
-		"block-opening-brace-newline-before": "always-single-line",
-		"block-opening-brace-space-after": "always-single-line",
-		"block-opening-brace-space-before": "always",
+		"stylistic/block-opening-brace-newline-after": "always",
+		"stylistic/block-opening-brace-newline-before": "always-single-line",
+		"stylistic/block-opening-brace-space-after": "always-single-line",
+		"stylistic/block-opening-brace-space-before": "always",
 
-		"color-hex-case": "lower",
+		"stylistic/color-hex-case": "lower",
 		"color-hex-length": "short",
 		"color-named": "never",
 
 		"comment-whitespace-inside": "always",
 
-		"declaration-bang-space-after": "never",
-		"declaration-bang-space-before": "always",
+		"stylistic/declaration-bang-space-after": "never",
+		"stylistic/declaration-bang-space-before": "always",
 
 		"declaration-block-no-redundant-longhand-properties": true,
-		"declaration-block-semicolon-newline-after": "always",
-		"declaration-block-semicolon-newline-before": "never-multi-line",
-		"declaration-block-semicolon-space-after": "always-single-line",
-		"declaration-block-semicolon-space-before": "never",
+		"stylistic/declaration-block-semicolon-newline-after": "always",
+		"stylistic/declaration-block-semicolon-newline-before": "never-multi-line",
+		"stylistic/declaration-block-semicolon-space-after": "always-single-line",
+		"stylistic/declaration-block-semicolon-space-before": "never",
 		"declaration-block-single-line-max-declarations": 1,
-		"declaration-block-trailing-semicolon": "always",
+		"stylistic/declaration-block-trailing-semicolon": "always",
 
-		"declaration-colon-space-after": "always",
-		"declaration-colon-space-before": "never",
+		"stylistic/declaration-colon-space-after": "always",
+		"stylistic/declaration-colon-space-before": "never",
 		"declaration-empty-line-before": [ "never", {
 			"ignore": [
 				"after-comment",
 				"inside-single-line-block"
 			]
 		} ],
+		"stylistic/declaration-colon-newline-after": "always-multi-line",
 		"declaration-no-important": true,
 
 		// `px` values disable accessibility browser setting of user font overrides and
@@ -84,39 +89,41 @@ module.exports = {
 		"font-weight-notation": "named-where-possible",
 
 		"function-disallowed-list": "rgb",
-		"function-comma-newline-after": "never-multi-line",
-		"function-comma-newline-before": "never-multi-line",
-		"function-comma-space-after": "always",
-		"function-comma-space-before": "never",
-		"function-max-empty-lines": 0,
+		"stylistic/function-comma-newline-after": "never-multi-line",
+		"stylistic/function-comma-newline-before": "never-multi-line",
+		"stylistic/function-comma-space-after": "always",
+		"stylistic/function-comma-space-before": "never",
+		"stylistic/function-max-empty-lines": 0,
 		"function-name-case": "lower",
-		"function-parentheses-newline-inside": "never-multi-line",
-		"function-parentheses-space-inside": "always",
+		"stylistic/function-parentheses-newline-inside": "never-multi-line",
+		"stylistic/function-parentheses-space-inside": "always",
 		"function-url-no-scheme-relative": true,
 		"function-url-quotes": "never",
-		"function-whitespace-after": "always",
+		"stylistic/function-whitespace-after": "always",
 
 		"length-zero-no-unit": true,
-		"linebreaks": "unix",
+		"stylistic/linebreaks": "unix",
 
-		"media-feature-colon-space-after": "always",
-		"media-feature-colon-space-before": "never",
-		"media-feature-name-case": "lower",
-		"media-feature-parentheses-space-inside": "always",
-		"media-feature-range-operator-space-after": "always",
-		"media-feature-range-operator-space-before": "always",
+		"stylistic/media-feature-colon-space-after": "always",
+		"stylistic/media-feature-colon-space-before": "never",
+		"stylistic/media-feature-name-case": "lower",
+		"stylistic/media-feature-parentheses-space-inside": "always",
+		"stylistic/media-feature-range-operator-space-after": "always",
+		"stylistic/media-feature-range-operator-space-before": "always",
 
-		"media-query-list-comma-newline-after": "always-multi-line",
-		"media-query-list-comma-newline-before": "never-multi-line",
-		"media-query-list-comma-space-after": "always-single-line",
-		"media-query-list-comma-space-before": "never",
+		"stylistic/media-query-list-comma-newline-after": "always-multi-line",
+		"stylistic/media-query-list-comma-newline-before": "never-multi-line",
+		"stylistic/media-query-list-comma-space-after": "always-single-line",
+		"stylistic/media-query-list-comma-space-before": "never",
 
 		"no-unknown-animations": true,
 
-		"number-leading-zero": "always",
-		"number-no-trailing-zeros": true,
+		"media-query-no-invalid": null,
 
-		"property-case": "lower",
+		"stylistic/number-leading-zero": "always",
+		"stylistic/number-no-trailing-zeros": true,
+
+		"stylistic/property-case": "lower",
 
 		"rule-empty-line-before": [
 			"always-multi-line", {
@@ -125,42 +132,42 @@ module.exports = {
 			}
 		],
 
-		"selector-attribute-brackets-space-inside": "always",
-		"selector-attribute-operator-space-after": "never",
-		"selector-attribute-operator-space-before": "never",
+		"stylistic/selector-attribute-brackets-space-inside": "always",
+		"stylistic/selector-attribute-operator-space-after": "never",
+		"stylistic/selector-attribute-operator-space-before": "never",
 		"selector-attribute-quotes": "always",
-		"selector-combinator-space-after": "always",
-		"selector-combinator-space-before": "always",
-		"selector-descendant-combinator-no-non-space": true,
+		"stylistic/selector-combinator-space-after": "always",
+		"stylistic/selector-combinator-space-before": "always",
+		"stylistic/selector-descendant-combinator-no-non-space": true,
 
-		"selector-list-comma-newline-after": "always",
-		"selector-list-comma-newline-before": "never-multi-line",
-		"selector-list-comma-space-after": "always-single-line",
-		"selector-list-comma-space-before": "never",
+		"stylistic/selector-list-comma-newline-after": "always",
+		"stylistic/selector-list-comma-newline-before": "never-multi-line",
+		"stylistic/selector-list-comma-space-after": "always-single-line",
+		"stylistic/selector-list-comma-space-before": "never",
 
-		"selector-max-empty-lines": 0,
+		"stylistic/selector-max-empty-lines": 0,
 		"selector-max-id": 0,
 		"selector-no-vendor-prefix": true,
-		"selector-pseudo-class-case": "lower",
-		"selector-pseudo-class-parentheses-space-inside": "always",
-		"selector-pseudo-element-case": "lower",
+		"stylistic/selector-pseudo-class-case": "lower",
+		"stylistic/selector-pseudo-class-parentheses-space-inside": "always",
+		"stylistic/selector-pseudo-element-case": "lower",
 		"selector-pseudo-element-colon-notation": "double",
 		"selector-type-case": "lower",
 		"selector-type-no-unknown": true,
 
-		"string-quotes": "single",
+		"stylistic/string-quotes": "single",
 
 		"time-min-milliseconds": 100,
 
-		"unit-case": "lower",
+		"stylistic/unit-case": "lower",
 
 		"value-keyword-case": "lower",
-		"value-list-max-empty-lines": 0,
+		"stylistic/value-list-max-empty-lines": 0,
 
-		"value-list-comma-newline-after": "never-multi-line",
-		"value-list-comma-newline-before": "never-multi-line",
-		"value-list-comma-space-after": "always-single-line",
-		"value-list-comma-space-before": "never"
+		"stylistic/value-list-comma-newline-after": "never-multi-line",
+		"stylistic/value-list-comma-newline-before": "never-multi-line",
+		"stylistic/value-list-comma-space-after": "always-single-line",
+		"stylistic/value-list-comma-space-before": "never"
 	},
 	"overrides": [
 		{
