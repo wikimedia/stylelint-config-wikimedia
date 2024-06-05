7da13036c4325e8d1ff3ceee64e2e5e9598c3754
Ed S <ejsanders@gmail.com>
2024-06-05 15:54:45 +0100

    Ignore 'css-nesting' errors in LESS files (#237)
    Fixes #236

--- support-basic-rules.js
+++ support-basic-rules.js
@@ -1,19 +1,24 @@
 'use strict';
 
-const overrides = require( './index' ).overrides;
+const utils = require( './src/utils' );
+const overrides = utils.getBasicOverrides();
 
 /* eslint-disable quotes, quote-props */
+const noUnsupportedBrowserFeatures = [ true, {
+	"browsers": require( 'browserslist-config-wikimedia/basic' ),
+	"severity": "warning",
+	"ignore": [ "css-not-sel-list" ],
+	"ignorePartialSupport": true
+} ];
+
+utils.extendNoUnsupportedBrowserFeaturesLess( overrides, noUnsupportedBrowserFeatures );
+
 module.exports = {
 	"plugins": [
 		"stylelint-no-unsupported-browser-features"
 	],
 	"rules": {
-		"plugin/no-unsupported-browser-features": [ true, {
-			"browsers": require( 'browserslist-config-wikimedia/basic' ),
-			"severity": "warning",
-			"ignore": [ "css-not-sel-list" ],
-			"ignorePartialSupport": true
-		} ],
+		"plugin/no-unsupported-browser-features": noUnsupportedBrowserFeatures,
 		// Must remain enabled as long as some of our "basic" browsers don't support https://caniuse.com/css-not-sel-list
 		"selector-not-notation": "simple"
 	},
